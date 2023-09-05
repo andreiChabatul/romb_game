dc7a9c0384d77ee2e747596f4af97da1938fb4d4
andreiChabatul <andreichabatul@gmail.com>
2023-09-05 21:38:12 +0300

    feat message chat

--- romb_game/src/app/const/index.ts
+++ romb_game/src/app/const/index.ts
@@ -1,5 +1,2 @@
-export const colorPlayerOne = '#F6E106';
-export const colorPlayerTwo = '#E20001';
-export const colorPlayerThree = '#42DD51';
-export const colorPlayerFour = '#0FCBF1';
+
 export const BASIC_URL = "http://localhost:3000/"
