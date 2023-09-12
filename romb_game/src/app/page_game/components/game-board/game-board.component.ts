a0f3825a4fa498c0fdde08cccfe82e2f876a13cd
Andrei Chabatul <andreichabatul@gmail.com>
2023-09-12 13:57:11 +0300

    fix turn player

--- romb_game/src/app/page_game/components/game-board/game-board.component.ts
+++ romb_game/src/app/page_game/components/game-board/game-board.component.ts
@@ -26,9 +26,10 @@ export class GameBoardComponent implements OnInit, OnDestroy {
       mergeMap((players) => this.userId$.pipe(
         map((userId) => {
           const turnId = players.filter(player => player.isTurn)[0];
-          console.log(turnId)
           if (turnId) {
             userId === turnId.id ? this.isTurn = true : this.isTurn = false;
+          } else {
+            this.isTurn = false;
           }
         })
       )
