0973e3fd5f6ba1d63efd55228983774c79f16a43
Andrei Chabatul <andreichabatul@gmail.com>
2023-11-24 16:13:19 +0300

    feat new item cell

--- romb_game/src/app/page-room/components/room-item/room-item.component.ts
+++ romb_game/src/app/page-room/components/room-item/room-item.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
-import { ACTIONS_BUTTON, EACTION_WEBSOCKET } from 'src/app/const/enum';
-import { InfoRoom } from 'src/app/types';
+import { Component, Input, OnChanges } from '@angular/core';
+import { EACTION_WEBSOCKET } from 'src/app/const/enum';
+import { Player, infoRoom } from 'src/app/types';
 import { WebSocketController } from 'src/app/webSocket/webSocket.controller';
 
 @Component({
@@ -9,17 +8,24 @@ import { WebSocketController } from 'src/app/webSocket/webSocket.controller';
   templateUrl: './room-item.component.html',
   styleUrls: ['./room-item.component.scss']
 })
-export class RoomItemComponent {
+export class RoomItemComponent implements OnChanges {
 
-  @Input() itemRoom: InfoRoom;
-  @Input() index: number;
-  textButton = ACTIONS_BUTTON.CREATE_ROOM;
+  @Input() infoRoom: infoRoom;
+  playerArr: (Player | null)[];
+  color: string;
 
-  constructor(private webSocketController: WebSocketController, private router: Router) { }
+  constructor(private webSocketController: WebSocketController) { }
 
-  joinRoom() {
-    this.webSocketController.sendMessage(EACTION_WEBSOCKET.JOIN_GAME, { idRoomJoin: this.itemRoom.idRoom });
-    this.router.navigate(['game']);
+  ngOnChanges(): void {
+    this.playerArr = [...this.infoRoom.players];
+    while (this.playerArr.length < this.infoRoom.maxPLayers) {
+      this.playerArr.push(null);
+    };
+  }
+
+  joinRoom(): void {
+
+    // this.webSocketController.sendMessage(EACTION_WEBSOCKET.JOIN_GAME, { idRoomJoin: this.infoRoom.idRoom });
   }
 
 }
