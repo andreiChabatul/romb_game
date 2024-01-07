b73d4012cc33906c4b33056886c736305fa0e480
andreiChabatul <andreichabatul@gmail.com>
2024-01-07 21:22:16 +0300

    fix bag

--- romb_game/src/app/page-room/components/room-item/room-item.component.ts
+++ romb_game/src/app/page-room/components/room-item/room-item.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable, map } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { EACTION_WEBSOCKET } from 'src/app/const/enum';
 import { fullPlayer, infoRoom } from 'src/app/types';
 import { AppStore } from 'src/app/types/state';
@@ -13,43 +13,44 @@ import { selectIdRoom, selectInfoUser } from 'src/store/selectors';
   templateUrl: './room-item.component.html',
   styleUrls: ['./room-item.component.scss']
 })
-export class RoomItemComponent implements OnChanges {
+export class RoomItemComponent implements OnChanges, OnDestroy {
 
   @Input() infoRoom: infoRoom;
   playerArr: (fullPlayer | null)[];
   isJoin: boolean;
-  infoUser$ = this.store.select(selectInfoUser);
+  idUser: string | undefined;
   idRoom$ = this.store.select(selectIdRoom);
+  subscription$: Subscription;
 
   constructor(private webSocketController: WebSocketController, private store: Store<AppStore>) { }
 
   ngOnChanges(): void {
+    this.subscription$ = this.store.select(selectInfoUser).subscribe((infoUser) => this.idUser = infoUser?.id);
+    this.isJoin = !!this.infoRoom.players.filter((player) => player.id === this.idUser).length;
     this.playerArr = [...this.infoRoom.players];
     while (this.playerArr.length < this.infoRoom.maxPlayers) {
       this.playerArr.push(null);
     };
   }
 
-  checkJoin(): Observable<boolean> {
-    return this.infoUser$.pipe(
-      map((infoUser) => {
-        let result: boolean = true;
-        this.infoRoom.players.forEach((player) => player.id === infoUser?.id ? result = false : '');
-        return result;
-      })
-    );
+  clickControl(): void {
+    this.isJoin ? this.leaveRoom() : this.joinRoom();
   }
 
-  joinRoom(): void {
+  private joinRoom(): void {
     this.store.dispatch(OpenModal({ payload: { modalState: 'joinGame', payload: this.infoRoom.idRoom } }));
   }
 
-  leaveRoom(): void {
+  private leaveRoom(): void {
     this.webSocketController.sendMessage(EACTION_WEBSOCKET.CONTROL_ROOM,
       {
         action: "leave",
         idRoom: this.infoRoom.idRoom
       });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription$.unsubscribe();
   }
 
 }
--- romb_game/src/app/page_game/pages/page-game/page-game.component.ts
+++ romb_game/src/app/page_game/pages/page-game/page-game.component.ts
@@ -4,6 +4,7 @@ import { selectAllPlayerArr, selectGameRoom } from 'src/store/selectors';
 import { AppStore, gameRoom } from 'src/app/types/state';
 import { fullPlayer } from 'src/app/types';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-game',
@@ -17,15 +18,17 @@ export class PageGameComponent implements OnInit, OnDestroy {
   gameRoom: gameRoom;
   subscription$: Subscription
 
-  constructor(private store: Store<AppStore>) { }
+  constructor(private store: Store<AppStore>, private router: Router) { }
 
   ngOnInit(): void {
     this.subscription$ = this.gameRoom$.subscribe((gameRoom) => this.gameRoom = gameRoom);
+    if (!this.gameRoom.idRoom) {
+      this.router.navigate(['rooms']);
+    };
   }
 
   ngOnDestroy(): void {
     this.subscription$.unsubscribe();
-
   }
 
   trackByFunction(index: number, item: fullPlayer) {
